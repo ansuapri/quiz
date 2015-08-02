f7d0fd9c971ab59205521967485cbc2498718e61
ansuapri <ansuapri@gmail.com>
2015-08-02 21:16:20 +0200

    Creacion comentarios

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,9 +1,11 @@
 ﻿var models = require('../models/models.js');
 
-// Autoload - factoriza el código si ruta incluye :quizId
+// Autoload - :id
 exports.load = function(req, res, next, quizId) {
-  models.Quiz.findById(quizId).then(
-    function(quiz) {
+  models.Quiz.find({
+		where:{id:Number(quizId)},
+		include:[{model:models.Comment}]
+	}).then(function(quiz){  
       if (quiz) {
         req.quiz = quiz;
         next();
